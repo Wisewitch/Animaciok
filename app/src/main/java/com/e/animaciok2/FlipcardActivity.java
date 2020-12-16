2afadd774a7e5dd9f76184e0c6d96cd840ab198c
Wisewitch <agnes.gazdik@gmail.com>
2020-12-17 00:21:23 +0100

    Shuffle és Flipcard Activik visza gombja

--- app/src/main/java/com/e/animaciok2/FlipcardActivity.java
+++ app/src/main/java/com/e/animaciok2/FlipcardActivity.java
@@ -2,13 +2,50 @@ package com.e.animaciok2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class FlipcardActivity extends AppCompatActivity {
+    Button butt_flipcard, butt_vissza_fc;
+    ImageView IV_card;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flipcard);
+
+        init();
+
+    }
+    private void flip() {
+    }
+
+    private void listeners() {
+        butt_flipcard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               flip();
+            }
+        });
+        butt_vissza_fc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent vissza = new Intent (FlipcardActivity.this, MainActivity.class);
+                startActivity(vissza);
+                finish();
+            }
+        });
+    }
+
+    private void init() {
+        butt_flipcard = findViewById(R.id.butt_flipcard);
+        butt_vissza_fc = findViewById(R.id.butt_vissza_fc);
+        IV_card = findViewById(R.id.IV_card);
+
+
+        //  adatbazis = new DBHelper(RogzitActivity.this);
     }
 }
--- app/src/main/java/com/e/animaciok2/ShufflecardActivity.java
+++ app/src/main/java/com/e/animaciok2/ShufflecardActivity.java
@@ -2,13 +2,53 @@ package com.e.animaciok2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ShufflecardActivity extends AppCompatActivity {
+    Button butt_shuffle, butt_vissza_s;
+    ImageView IV_cb1, IV_cb2, IV_cb3, IV_cb4, IV_cb5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shufflecard);
+
+        init();
+
+    }
+    private void shuffle() {
+    }
+
+    private void listeners() {
+        butt_shuffle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shuffle();
+            }
+        });
+        butt_vissza_s.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent vissza = new Intent (ShufflecardActivity.this, FlipcardActivity.class);
+                startActivity(vissza);
+                finish();
+            }
+        });
+    }
+
+    private void init() {
+        butt_shuffle = findViewById(R.id.butt_shuffle);
+        butt_vissza_s = findViewById(R.id. butt_vissza_s);
+        IV_cb1 = findViewById(R.id.IV_cb1 );
+        IV_cb2 = findViewById(R.id.IV_cb2);
+        IV_cb3 = findViewById(R.id.IV_cb3 );
+        IV_cb4 = findViewById(R.id.IV_cb4 );
+        IV_cb5 = findViewById(R.id.IV_cb5 );
+
+      //  adatbazis = new DBHelper(RogzitActivity.this);
     }
 }
